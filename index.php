f6a89ea65ae85aaaa4800946cd850de18b7691d0
KanchanKulkarni <b.kanchankulkarni@gmail.com>
2017-05-11 05:45:21 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+Welcome  <?php echo $_POST["name"]; ?> <br>
+	<p>Please fill the details below</p>
+	<form action="" method="post">
+Full Name: <input type="text" name="full name">
+<br />
+<br />
+Email ID: <input type="text" name="mail">
+<br />
+		<br />
+Qualification: <input type="text" name="quali">
+<br />
+		<br />
+Hobby: <input type="text" name="hobby">
+<br />
+		<br />
+<input type="submit">
+</form>
+</body>
+</html>
